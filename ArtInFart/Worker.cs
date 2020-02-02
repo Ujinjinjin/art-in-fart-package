68d89f64dc638a0d3eebf80b86f7d2d13292864f
Camille Galladjov <ujinjinjin@outlook.com>
2020-02-02 12:52:02 +0300

    initial files

---
+++ ArtInFart/Worker.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ArtInFart
+{
+    public class Worker
+    {
+        public string GiveMePhrase()
+        {
+            return "I put art in fart";
+        }
+    }
+}
